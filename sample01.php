dd7206f33dff49bc12be22bf83ec60e94c1ae2a2
joycat0138 <77442417+joycat0138@users.noreply.github.com>
2024-06-23 23:10:20 +0900

    サンププソースコード01を追加

--- sample01.php
+++ sample01.php
@@ -1,1 +1,36 @@
-sample01.php
+<?php
+
+$array = [
+	'is_aaaa'       => $this->navCurrentClass($page_slug, 'aaaaaaaa'),
+	'is_bbbbbbb'       => $this->navCurrentClass($page_slug, ['bbbbb','ccccc','dddd','ffff','eeeee']),
+	'is_ccccccccc'       => $this->navCurrentClass($page_slug, 'gggggg'),
+	'is_dddd'       => $this->navCurrentClass($page_slug, 'hhhhh'),
+	'is_ffff'       => $this->navCurrentClass($page_slug, 'iiii'),
+	'is_g' => $this->navCurrentClass($page_slug, 'jjjjjjj'),
+	'is_hhhh'   => $this->navCurrentClass($page_slug, ['kkkkk','llllll','mmmmmmm','nnnnnnn','ooooooo','ppppp']),
+	'is_iiiiiiiii'       => $this->navCurrentClass($page_slug, ['qqqqq','rrrr','sssss','tttttt']),
+	'is_jjjjjj' => $this->navCurrentClass($page_slug, 'uuuuuuu')
+];
+
+function navCurrentClass($page_slug, $nav_slug){
+	if( is_array($nav_slug) ){
+
+		$is_match = false;
+		foreach ($nav_slug as $slug) {
+			if (strpos($page_slug, $slug) !== false) {
+				$is_match = true;
+				break;
+			}
+		}
+
+		if ($is_match) {
+			return 'is-active';
+		}
+
+		return '';
+	} elseif ( strpos($page_slug, $nav_slug) !== false ){
+		return 'is-active';
+	}
+
+	return '';
+}
